e403467a17bcf8c6c17c320d1538d179d6696b34
Debjoti Mallick <debjoti97@gmail.com>
2022-03-01 12:13:02 +0530

    Project of Day 18

---
+++ Day 11-20/Day-018/main.py
@@ -0,0 +1,33 @@
+import random
+import turtle
+from turtle import *
+tim = Turtle()
+turtle.colormode(255)
+# tim.hideturtle()
+# color_list = [(249, 248, 248), (237, 241, 245), (238, 246, 244), (249, 243, 247), (1, 12, 31), (53, 25, 17)]
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+
+tim.penup()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+tim.pendown()
+
+for dots in range(1, 100):
+    tim.dot(20, random.choice(color_list))
+    tim.penup()
+    tim.forward(50)
+    tim.pendown()
+
+    if dots % 10 == 0:
+        tim.penup()
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+        tim.pendown()
+
+
+screen = turtle.Screen()
+screen.exitonclick()
